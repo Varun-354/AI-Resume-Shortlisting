4670ace763bd336f9087bb4283c0c7e155ed0d64
{varun} <{abc@gmail.com}>
2026-02-01 13:46:55 +0530

    Step 4 : text preprocessing added

--- src/main.py
+++ src/main.py
@@ -10,3 +10,19 @@ with pdfplumber.open(resume_path) as pdf:
         if text:
             print(text)
 
+
+import re
+
+def preprocess_text(text):
+    text = text.lower()
+    text = re.sub(r'[^a-z\s]', ' ', text)
+    text = re.sub(r'\s+', ' ', text)
+    return text.strip()
+
+with pdfplumber.open(resume_path) as pdf:
+    for page in pdf.pages:
+        raw_text = page.extract_text()
+        if raw_text:
+            clean_text = preprocess_text(raw_text)
+            print(clean_text)
+
